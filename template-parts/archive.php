81be1bc9877d541c99f64014f8362e614e530cdd
James FL <jlacandula@gmail.com>
2022-03-19 08:05:56 +0800

    Add redirect to main resources archive page is resource_category

---
+++ template-parts/archive.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// redirect to main resources page with custom search and filter if taxnomy archive is resource_category
+$queried_oject = get_queried_object();
+$cat_id = $queried_oject->term_id;
+$cat = $queried_oject->taxonomy;
+if ( $cat === 'resource_category'  ) {
+	$url = '/resources/jsf/epro-posts/tax/resource_category:' . $cat_id . '/';
+	wp_redirect( $url );
+	exit;
+}
+?>
+<main class="site-main" role="main">
+
+	<?php if ( apply_filters( 'hello_elementor_page_title', true ) ) : ?>
+		<header class="page-header">
+			<?php
+			the_archive_title( '<h1 class="entry-title">', '</h1>' );
+			the_archive_description( '<p class="archive-description">', '</p>' );
+			?>
+		</header>
+	<?php endif; ?>
+	<div class="page-content">
+		<?php
+		while ( have_posts() ) {
+			the_post();
+			$post_link = get_permalink();
+			?>
+			<article class="post">
+				<?php
+				printf( '<h2 class="%s"><a href="%s">%s</a></h2>', 'entry-title', esc_url( $post_link ), esc_html( get_the_title() ) );
+				printf( '<a href="%s">%s</a>', esc_url( $post_link ), get_the_post_thumbnail( $post, 'large' ) );
+				the_excerpt();
+				?>
+			</article>
+		<?php } ?>
+	</div>
+
+	<?php wp_link_pages(); ?>
+
+	<?php
+	global $wp_query;
+	if ( $wp_query->max_num_pages > 1 ) :
+		?>
+		<nav class="pagination" role="navigation">
+			<?php /* Translators: HTML arrow */ ?>
+			<div class="nav-previous"><?php next_posts_link( sprintf( __( '%s older', 'hello-elementor' ), '<span class="meta-nav">&larr;</span>' ) ); ?></div>
+			<?php /* Translators: HTML arrow */ ?>
+			<div class="nav-next"><?php previous_posts_link( sprintf( __( 'newer %s', 'hello-elementor' ), '<span class="meta-nav">&rarr;</span>' ) ); ?></div>
+		</nav>
+	<?php endif; ?>
+</main>
